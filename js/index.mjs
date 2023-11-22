e099cdc836570517de68208c31d14c9330defbcd
chalund <114383740+chalund@users.noreply.github.com>
2023-11-22 13:17:45 +0100

    updates

--- js/index.mjs
+++ js/index.mjs
@@ -1,15 +1,11 @@
+console.log("js/index")
+
 import { toggleLoginDisplay } from "./loginBtn.mjs";
 toggleLoginDisplay()
 
 
-
 import { registerFormListener } from "./handlers/register.mjs"; 
 import { loginFormListener} from "./handlers/login.mjs";
-import { createPost } from "./api/posts/create.mjs";
-
-
-
-
 
 const path = location.pathname;
 
@@ -19,11 +15,3 @@ if( path === '/profile/login/index.html') {
     registerFormListener()
 }
 
-
-
-
-// createPost({
-//     title: "Garmin Venu 2S",
-//     media: "https://www.elkjop.no/image/dv_web_D180001002732020/286782/garmin-venu-2s-gps-smartklokke-rosegullhvit--pdp_zoom-3000--pdp_main-960.jpg",
-//     endsAt: "2023-11-23T12:30:00.000Z",
-// })
