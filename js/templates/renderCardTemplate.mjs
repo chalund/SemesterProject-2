d1bdd1b73983e03ffd341472d6ed4450a5309702
chalund <114383740+chalund@users.noreply.github.com>
2023-12-15 15:20:56 +0100

    adds loader

--- js/templates/renderCardTemplate.mjs
+++ js/templates/renderCardTemplate.mjs
@@ -1,11 +1,9 @@
-import { postTemplate } from "./cardTemplate.mjs"
-import { getActivePosts, getPosts} from "../api/posts/getPost.mjs"; 
-
+import { postTemplate } from "./cardTemplate.mjs";
+import { getPosts } from "../api/posts/getPost.mjs";
 
 export async function renderCardTemplate(containerId, posts) {
     try {
-        // const posts = await getActivePosts();
-
+        const loader = document.querySelector(".loader");
         const container = document.querySelector(`#${containerId}`);
 
         if (!container) {
@@ -16,22 +14,19 @@ export async function renderCardTemplate(containerId, posts) {
             throw new Error('Posts retrieval failed or the data format is incorrect.');
         }
 
-    
-
         posts.forEach(post => {
             const postContent = postTemplate(post);
             container.append(postContent);
         });
+
+        // Hide loader after rendering
+        loader.style.display = "none";
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-
-document.addEventListener('DOMContentLoaded', async() => {
+document.addEventListener('DOMContentLoaded', async () => {
     const posts = await getPosts();
     renderCardTemplate('auctionPosts', posts);
 });
-
-
-
